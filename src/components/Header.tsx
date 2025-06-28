ce0c4874ba3b5a0afed46e92fb2f4b71e39390ef
Hoang Lee <hoanglee26222@gmail.com>
2025-06-28 14:58:11 +0700

    up check phantom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -317,7 +317,7 @@ const Header = () => {
             <div className="px-4 py-2">
               {/* <div className="text-xs text-gray-500 mb-2">{t('header.navigation')}</div> */}
               <div className="flex flex-col gap-1">
-                {tabs.map((tab) => (
+                {tabs.filter((tab) => tab.isActive).map((tab) => (
                   <Link
                     className={cn(
                       'flex items-center gap-2 px-3 py-2 rounded-lg text-xs text-neutral hover:bg-gray-100/10 bg-black/40 transition-colors',
