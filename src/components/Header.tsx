50e0e13c4ab10caf184a1e2b940a395c443e7679
Hoang Lee <hoanglee26222@gmail.com>
2025-07-02 18:36:18 +0700

    up mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,8 +28,8 @@ const Header = () => {
   const tabs = [
     {
       id: 'overview', href: '/overview', label: t('header.overview'), icon: '📊', isActive: true, dropdown: [
-        { id: 'mmp-info', href: '/mmp-info', label: t('header.mmpInfo'), icon: '📊' },
-        { id: 'mpb-info', href: '/mpb-info', label: t('header.mpbInfo'), icon: '📊' },
+        { id: 'mmp-info', href: '/mmp-info', label: t('header.mmpInfo'), icon: '📊', isActive: true },
+        { id: 'mpb-info', href: '/mpb-info', label: t('header.mpbInfo'), icon: '📊', isActive: true },
       ]
     },
     { id: 'swap', href: '/swap', label: t('header.swap'), icon: <ArrowDownUp className='w-3 h-3 sm:w-4 sm:h-4' />, isActive: true },
@@ -341,7 +341,7 @@ const Header = () => {
             <div className="px-4 py-2">
               {/* <div className="text-xs text-gray-500 mb-2">{t('header.navigation')}</div> */}
               <div className="flex flex-col gap-1">
-                {tabs.filter((tab) => tab.isActive).map((tab) => (
+                {[...tabs, ...tabs[0].dropdown].filter((tab) => tab.isActive).map((tab) => (
                   <Link
                     className={cn(
                       'flex items-center gap-2 px-3 py-2 rounded-lg text-xs text-neutral hover:bg-gray-100/10 bg-black/40 transition-colors',
